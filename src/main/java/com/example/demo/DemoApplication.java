09d8e16e7d3debdc0c769c9b5634fed47a1ca006
Sándor Negyeliczky <sandor.negyeliczky@gmail.com>
2020-01-29 17:44:13 +0100

    first

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DemoApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
     }
+
 
 }
